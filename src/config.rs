c018b9ec928fce37153e7dba59ef5b3a9a2817cb
David Kleingeld <dskleingeld@gmail.com>
2019-04-01 01:54:05 +0200

    adds algoritme to determine read_into_packages params

--- src/config.rs
+++ src/config.rs
@@ -3,7 +3,7 @@
 //pub const MAX_BYTES_PER_PACKAGE: usize = 80;
 //pub const MAX_BYTES_PER_PACKAGE: usize = 264;
 
-pub const MAX_SINGLE_READ: usize = 10_000; //10Kb
+pub const MAX_READ_MEMORY: usize = 10_000; //10Kb
 pub const MAX_PACKAGE_SIZE: usize = 10_000; //10Kb must be smaller or equal to max package size
 
 

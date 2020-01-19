6cad6753471a501f894c3c8651e199605260835e
dskleingeld <dskleingeldgmail.com>
2020-01-19 16:52:30 +0100

    moves constants token, domain and port to arguments

--- releaseToRemoteHost.sh
+++ releaseToRemoteHost.sh
@@ -1,4 +1,4 @@
-cross build --target=armv7-unknown-linux-gnueabihf --release --features plotting stable
+cross build --target=armv7-unknown-linux-gnueabihf --release --features "plotting stable"
 rsync -vh --progress \
     target/armv7-unknown-linux-gnueabihf/release/dataserver \
     pi@192.168.1.10:/home/pi/dataserver/stable/dataserver \

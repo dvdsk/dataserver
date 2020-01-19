4dc9e4a693cc0e38e49294366a5136db1d2d5848
dskleingeld <dskleingeldgmail.com>
2020-01-19 14:08:43 +0100

    removes release deploy script, modifies target location to dev folder

--- debugToRemoteHost.sh
+++ debugToRemoteHost.sh
@@ -2,4 +2,4 @@
 cross build --target=armv7-unknown-linux-gnueabihf --features plotting
 rsync -vh --progress \
     target/armv7-unknown-linux-gnueabihf/debug/dataserver \
-    pi@192.168.1.10:/home/pi/dataserver/dataserver \
+    pi@192.168.1.10:/home/pi/dataserver/dev/dataserver \
--- optimisedToRemoteHost.sh
+++ optimisedToRemoteHost.sh
@@ -1,4 +1,4 @@
 cross build --target=armv7-unknown-linux-gnueabihf --release
 rsync -vh --progress \
     target/armv7-unknown-linux-gnueabihf/release/dataserver \
-    pi@192.168.1.10:/home/pi/dataserver/dataserver \
+    pi@192.168.1.10:/home/pi/dataserver/dev/dataserver \

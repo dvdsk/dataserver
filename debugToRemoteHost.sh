93efab0ca70dc50714e7eec3003c90b4c72c1692
dskleingeld <dskleingeldgmail.com>
2020-01-19 14:04:17 +0100

    adds stable feature to deploy script

--- debugToRemoteHost.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-cross build --target=armv7-unknown-linux-gnueabihf --features plotting
-rsync -vh --progress \
-    target/armv7-unknown-linux-gnueabihf/debug/dataserver \
-    pi@192.168.1.10:/home/pi/dataserver/dataserver \
--- optimisedToRemoteHost.sh
+++
@@ -1,4 +0,0 @@
-cross build --target=armv7-unknown-linux-gnueabihf --release
-rsync -vh --progress \
-    target/armv7-unknown-linux-gnueabihf/release/dataserver \
-    pi@192.168.1.10:/home/pi/dataserver/dataserver \
